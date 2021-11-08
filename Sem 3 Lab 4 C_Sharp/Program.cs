2d3bae16669f260df8b383ed33f77b2b38a8104b
Вадим Шуваев <shuvaev.v.6@outlook.com>
2021-11-08 16:57:01 +0700

    Добавьте файлы проекта.

---
+++ Sem 3 Lab 4 C_Sharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Sem_3_Lab_4_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
