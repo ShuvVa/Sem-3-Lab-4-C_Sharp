18bd6f46b52c1ddfe7afd9681cd487fdb090b5bb
Вадим Шуваев <shuvaev.v.6@outlook.com>
2021-11-08 23:13:52 +0700

    Код готов на >10% Список изменений: - Добавлен исходный код с С++ - Недописан класс Logger - Не сделан родительский класс IError - Не сделан дочерний класс MathIError - Не сделан дочерний класс InputIError - Не сделан класс Game - Не сделан класс FracNum

---
+++ Sem 3 Lab 4 C_Sharp/Logger.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Logger
+{
+    class Logger
+    {
+        public bool Enable;
+        
+        public Logger(string str, bool enable = false)
+        {
+            Enable = enable;
+            if (Enable) Console.WriteLine($"\nConstruct {str}\n");
+        }
+            
+    }
+}
