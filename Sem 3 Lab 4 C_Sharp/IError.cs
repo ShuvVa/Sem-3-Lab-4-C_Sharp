fe3a3ea2eb14007ebc0c29825e9134237a64e617
Вадим Шуваев <shuvaev.v.6@outlook.com>
2021-11-09 10:41:08 +0700

    Список изменений: - Дописан абстрактный класс IError - Дописан наследуемый класс MathIError - Дописан наследуемый класс InputIError

--- Sem 3 Lab 4 C_Sharp/IError.cs
+++ Sem 3 Lab 4 C_Sharp/IError.cs
@@ -3,10 +3,67 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace IError
 {
-    class IError
+    abstract class IError
     {
+        protected string m_IError;
+        
+        public IError(string error)
+        {
+            m_IError = error;
+        }
+
+        public string what()
+        {
+            return m_IError;
+        }
+
+        ~IError(){
+            string path = @"C:\ErrorLog\Error.txt";
+            string txt = ($"{m_IError}\n");
+            using (StreamWriter sw = new StreamWriter(path, false, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(txt);
+            }
+            using (StreamWriter sw = new StreamWriter(path, true, System.Text.Encoding.Default))
+            {
+                sw.WriteLine(txt);
+            }
+
+            
+        }
+    }
+
+    class MathIError : IError
+    {
+        private string m_mathIError;
+        public MathIError(string error, int a, int b, char op) : base (error)
+        {
+            m_mathIError = a + " " + op + " " + b;
+        }
+
+        public new string what()
+        {
+            string error = m_IError + " : " + m_mathIError;
+            return error;
+        }
+    }
+
+    class InputIError : IError
+    {
+        private string m_inputIError;
+        public InputIError(string error, string inputtext) : base(error)
+        {
+            m_inputIError = inputtext;
+        }
+
+        public new string what()
+        {
+            string error = m_IError + " : " + m_inputIError;
+            return error;
+        }
     }
 }
