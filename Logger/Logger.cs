0f54e601fb3d85b04d86297e4a2187817a39ac1a
Shuvaev_Vadim <shuvaev.v.6@gmail.com>
2022-08-28 20:42:31 +0700

    Changes

---
+++ Logger/Logger.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace LoggerLib
+{
+    public class Logger : IDisposable
+    {
+
+        static public bool Enable;
+
+        public Logger(string str)
+        {
+            this.nameClass = str;
+            if (Enable) Console.Write($"\nConstruct {this.nameClass}\n");
+        }
+
+        public void OutFuctName(string name)
+        {
+            if (Enable) Console.Write($"Function {name} begin to work.");
+        }
+
+        public void Dispose()
+        {
+            if (Enable) Console.Write($"\nDestruct {nameClass}\n");
+        }
+
+        string nameClass;
+    }
+}
